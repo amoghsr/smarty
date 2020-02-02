a4479ad8b2143fe7ec9d50a824fa66728272dee2
Amogh Srivastava <amoghsrivastava28@yahoo.com>
2020-02-02 11:22:32 +0400

    Fixed alignment

--- lib/devices_controller.dart
+++ lib/devices_controller.dart
@@ -57,6 +57,7 @@ class _DevicesControllerState extends State<DevicesController> {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
               Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   Text(
                     roomName,
