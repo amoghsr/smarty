1ec5e9ace15482ab4e2262d9385cb43dab5fb9a5
Amogh Srivastava <amoghsrivastava28@yahoo.com>
2020-03-14 17:12:16 +0400

    Fingerprint and FaceID (local authentication) files. Still WIP.

---
+++ lib/services/local_authentication_service.dart
@@ -0,0 +1,33 @@
+import 'package:local_auth/local_auth.dart';
+import 'package:flutter/services.dart';
+
+class LocalAuthenticationService {
+  /* _auth variable simply instantiates the LocalAuthentication
+    library, nothing special here. */
+  final _auth = LocalAuthentication();
+  /*
+  bool _isProtectionEnabled keeps track of the respective setting,
+  in a real-world app, it should be stored in the shared preferences.
+   */
+  bool _isProtectionEnabled = false;
+
+  bool get isProtectionEnabled => _isProtectionEnabled;
+  set isProtectionEnabled(bool enabled) => _isProtectionEnabled = enabled;
+
+  // isAuthenticated variable will keep track of the authentication
+  bool isAuthenticated = false;
+
+  Future<void> authenticate() async {
+    if (_isProtectionEnabled) {
+      try {
+        isAuthenticated = await _auth.authenticateWithBiometrics(
+          localizedReason: 'authenticate to access',
+          useErrorDialogs: true,
+          stickyAuth: true,
+        );
+      } on PlatformException catch (e) {
+        print(e);
+      }
+    }
+  }
+}
