3c5836c8d8df59f66f6a6a1a48560f007234fa89
Amogh Srivastava <amoghsrivastava28@yahoo.com>
2020-04-15 18:10:38 +0400

    Added colors to routine model

--- lib/models/routineModel.dart
+++ lib/models/routineModel.dart
@@ -1,36 +1,41 @@
 import 'package:flutter/cupertino.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter/material.dart';
 
 class Routine {
   Icon icon;
   String routineName;
   String routineDesc;
+  Color routineColor;
 
-  Routine({this.icon, this.routineName, this.routineDesc});
+  Routine({this.icon, this.routineName, this.routineDesc, this.routineColor});
 }
 
 List<Routine> routines = [
   Routine(
     icon: Icon(
       FontAwesomeIcons.walking,
-      color: Color(0xFFf8931f),
+      color: Colors.blue[100],
     ),
-    routineName: 'I\'m Leaving',
+    routineColor: Colors.blue,
+    routineName: 'Exiting',
     routineDesc: 'On Tap',
   ),
   Routine(
     icon: Icon(
       FontAwesomeIcons.film,
-      color: Color(0xFFf8931f),
+      color: Colors.red[100],
     ),
-    routineName: 'Movie Mode',
+    routineColor: Colors.red[600],
+    routineName: 'Movie',
     routineDesc: 'On Tap',
   ),
   Routine(
     icon: Icon(
       FontAwesomeIcons.solidMoon,
-      color: Color(0xFFf8931f),
+      color: Colors.deepPurple[100],
     ),
+    routineColor: Colors.deepPurpleAccent,
     routineName: 'Night Time',
     routineDesc: 'From 9:00 PM to 6:00 AM',
   ),
