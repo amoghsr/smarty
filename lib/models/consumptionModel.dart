6bde959757591065ba2acd0224e3e2ce1d00ea7d
Piya Gehi <pjg1@hw.ac.uk>
2020-04-18 21:28:44 +0400

    modified generationModel and consumptionModel

--- lib/models/consumptionModel.dart
+++ lib/models/consumptionModel.dart
@@ -1,6 +1,7 @@
 class Consumption {
-  int daily_total = 0;
+  int dailyTotal = 0;
   Map<String, int> monthly = new Map<String, int>();
   Map<String, int> weekly = new Map<String, int>();
   Map<String, Map<String, int>> daily = new Map<String, Map<String, int>>();
+  Map<String, Map<String, int>> devicesDaily = new Map<String, Map<String, int>>();
 }
--- lib/models/generationModel.dart
+++ lib/models/generationModel.dart
@@ -1,5 +1,5 @@
 class Generation {
-  int daily_total = 0;
+  int dailyTotal = 0;
   Map<String, int> monthly = new Map<String, int>();
   Map<String, int> weekly = new Map<String, int>();
   Map<String, Map<String, int>> daily = new Map<String, Map<String, int>>();
