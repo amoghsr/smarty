add34962cf2eb311efc2d742b92da2533ea4707e
Piya Gehi <pjg1@hw.ac.uk>
2020-04-18 21:27:21 +0400

    Added generationModel.dart and consumptionModel.dart

---
+++ lib/models/consumptionModel.dart
@@ -0,0 +1,6 @@
+class Consumption {
+  int daily_total = 0;
+  Map<String, int> monthly = new Map<String, int>();
+  Map<String, int> weekly = new Map<String, int>();
+  Map<String, Map<String, int>> daily = new Map<String, Map<String, int>>();
+}
