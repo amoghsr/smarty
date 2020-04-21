9d993f4bd97d015d7d41b949cd2b5b1fd4231044
Amogh Srivastava <amoghsrivastava28@yahoo.com>
2020-04-21 16:23:35 +0400

    Welcome screen added

---
+++ lib/authenticate/greeting.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:smarty/authenticate/greeting_screens/getting_started.dart';
+import 'package:smarty/authenticate/sign_in.dart';
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:provider/provider.dart';
+import 'package:smarty/models/themeModel.dart';
+
+class GreetingPage extends StatefulWidget {
+  @override
+  _GreetingPageState createState() => _GreetingPageState();
+}
+
+class _GreetingPageState extends State<GreetingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            colorFilter: ColorFilter.mode(Colors.green, BlendMode.difference),
+            image: AssetImage('assets/images/back.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 30.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Hero(
+                tag: 'logo',
+                child: Material(
+                  color: Colors.green.withOpacity(0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        height: 50,
+                        width: 50,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            colorFilter: ColorFilter.mode(
+                                Theme.of(context)
+                                    .scaffoldBackgroundColor.withOpacity(0),
+                                BlendMode.difference),
+                            image: AssetImage(
+                                'assets/images/logo_white.png'),
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 20.0),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text('HOMI', style: TextStyle(letterSpacing: 10.0, fontWeight: FontWeight.bold, color: Colors.white),),
+                          Text('CONTROL', style: TextStyle(letterSpacing: 10.0, fontWeight: FontWeight.bold, color: Colors.white),),
+                          Text('SYSTEM', style: TextStyle(letterSpacing: 10.0, fontWeight: FontWeight.bold, color: Colors.white),),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: Text(
+                  'Welcome to the future of smart home control',
+                  style: TextStyle(
+                    fontFamily: 'Poppins',
+                    height: 1.2,
+                    fontSize: 36.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 16.0),
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          CupertinoPageRoute(
+                            builder: (context) => SignIn(),
+                          ),
+                        );
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(8.0),
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Colors.black.withOpacity(0.2),
+                                  offset: Offset(0,2),
+                                  blurRadius: 6.0
+                              )
+                            ]
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text(
+                                'I already have an account',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Column(
+                    children: <Widget>[
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            CupertinoPageRoute(
+                              builder: (context) => GettingStartedScreen(),
+                            ),
+                          );
+                        },
+                        child: Text(
+                          'Join as a new home member',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            letterSpacing: 0.4
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
